48b4e8589b345c56838c6e1e2114ed5e71146a43
Chen <chenzhe@nichdm02149820.nih.gov>
2020-05-05 21:00:53 -0400

    auc

--- R/AUCfunctions.R
+++ R/AUCfunctions.R
@@ -8,7 +8,8 @@
 #' @param ngrid number of equal-spaced FPR values on unit interval to compute TRP.
 #' @return either a vector of auc estimates or (if ROC = TRUE) a list of a vector of auc estimates and dataframe of FPR and TPR.
 #' @export
-auc <- function(x1=20,x2=NULL,fit,model="PH",ROC=FALSE,ngrid=100) {
+auc <- function(x1=20,x2=NULL,fit,model = c("PH", "PrevH"),ROC=FALSE,ngrid=100) {
+  model <- match.arg(model)
   k <- length(fit$coef)
   myformula <- switch((k+1)/2,"1/(1+exp(b1))","1/(1+exp(b1+x1*b3))","1/(1+exp(b1+x1*b4+x2*b5))","1/(1+exp(b1+x1*b4+x2*b5+x1*x2*b7))")
   auc <- car::deltaMethod(fit,myformula,parameterNames=paste("b",1:length(fit$coef),sep=""))
@@ -17,8 +18,6 @@ auc <- function(x1=20,x2=NULL,fit,model="PH",ROC=FALSE,ngrid=100) {
        return(list(auc=auc,roc=data.frame(FPR=seq(0,1,l=100),TPR=seq(0,1,l=ngrid)**(1/auc$Est-1))))
     } else if (model == "PrevH") {
        return(list(auc=auc,roc=data.frame(FPR=seq(0,1,l=100),TPR=1-(1-seq(0,1,l=ngrid))**(1/(1/auc$Est-1)))))
-    } else {
-
     }
   } else {
     return(auc)
