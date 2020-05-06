db8e2521790e01e915e432b4f8f77658664ac57d
Chen <chenzhe@nichdm02149820.nih.gov>
2020-05-05 20:56:53 -0400

    auc

--- R/AUCfunctions.R
+++ R/AUCfunctions.R
@@ -4,15 +4,22 @@
 #' @param x2 a value of covariate x2 at which to estimate auc and ROC curve. Can be absent.
 #' @param fit a coxph/croc object of model fit.
 #' @param ROC logical, if TRUE, ROC estimates are produced in addition to AUC. Default is FALSE.
+#' @param model a character string specifying the model for the conave ROC curve, one of PH and PrevH.
 #' @param ngrid number of equal-spaced FPR values on unit interval to compute TRP.
 #' @return either a vector of auc estimates or (if ROC = TRUE) a list of a vector of auc estimates and dataframe of FPR and TPR.
 #' @export
-auc <- function(x1=20,x2=NULL,fit,ROC=FALSE,ngrid=100) {
+auc <- function(x1=20,x2=NULL,fit,model="PH",ROC=FALSE,ngrid=100) {
   k <- length(fit$coef)
   myformula <- switch((k+1)/2,"1/(1+exp(b1))","1/(1+exp(b1+x1*b3))","1/(1+exp(b1+x1*b4+x2*b5))","1/(1+exp(b1+x1*b4+x2*b5+x1*x2*b7))")
   auc <- car::deltaMethod(fit,myformula,parameterNames=paste("b",1:length(fit$coef),sep=""))
   if(ROC) {
-    return(list(auc=auc,roc=data.frame(FPR=seq(0,1,l=100),TPR=seq(0,1,l=ngrid)**(1/auc$Est-1))))
+    if (model == "PH") {
+       return(list(auc=auc,roc=data.frame(FPR=seq(0,1,l=100),TPR=seq(0,1,l=ngrid)**(1/auc$Est-1))))
+    } else if (model == "PrevH") {
+       return(list(auc=auc,roc=data.frame(FPR=seq(0,1,l=100),TPR=1-(1-seq(0,1,l=ngrid))**(1/(1/auc$Est-1)))))
+    } else {
+
+    }
   } else {
     return(auc)
   }
